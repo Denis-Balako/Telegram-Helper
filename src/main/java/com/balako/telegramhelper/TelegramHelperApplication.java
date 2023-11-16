36ccd9eb53ed8d665f1f4fb058162c10046dced4
Denis Balako <denisarius2000s@gmail.com>
2023-11-16 12:33:04 +0200

    init commit

---
+++ src/main/java/com/balako/telegramhelper/TelegramHelperApplication.java
@@ -0,0 +1,13 @@
+package com.balako.telegramhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/balako/telegramhelper/TelegramHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balako.telegramhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
